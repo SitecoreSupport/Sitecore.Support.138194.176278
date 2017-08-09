d257c20c241b1464dae5557deae5448fb7a3d296
Oleg Burov <oleg.burov@outlook.com>
2017-08-08 18:41:23 -0700

    implement patch #176278.

--- src/Sitecore.Support.138194.176278/Data/SqlServer/SqlServerClientDataStore.cs
+++ src/Sitecore.Support.138194.176278/Data/SqlServer/SqlServerClientDataStore.cs
@@ -1,6 +1,7 @@
 ﻿using Sitecore.Caching;
 using Sitecore.Data.DataProviders.Sql;
 using Sitecore.Diagnostics.PerformanceCounters;
+using System;
 using System.Reflection;
 
 namespace Sitecore.Support.Data.SqlServer
@@ -34,5 +35,23 @@ namespace Sitecore.Support.Data.SqlServer
         }
       }
     }
+
+    protected override void CompactData()
+    {
+      var batchSize = Configuration.Settings.LinkDatabase.MaximumBatchSize;
+      DateTime time = DateTime.UtcNow - ObjectLifetime;
+
+      string sql = "DELETE TOP ({2}batchSize{3}) FROM {0}ClientData{1} WHERE {0}Accessed{1} < {2}maxDate{3} SELECT @@ROWCOUNT";
+      object[] parameters = new object[] { "batchSize", batchSize, "maxDate", time };
+
+      var affectedRows = 0;
+
+      do
+      {
+        affectedRows = this._api.Execute(sql, parameters);
+      }
+      while (affectedRows > 0);
+    }
+
   }
 }
