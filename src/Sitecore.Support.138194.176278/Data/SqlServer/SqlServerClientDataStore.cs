97e0733e3f9be262a676be7317b7670e56b3a56a
Oleg Burov <oleg.burov@outlook.com>
2017-08-08 18:39:30 -0700

    Implement patch #138194.

---
+++ src/Sitecore.Support.138194.176278/Data/SqlServer/SqlServerClientDataStore.cs
@@ -0,0 +1,38 @@
+﻿using Sitecore.Caching;
+using Sitecore.Data.DataProviders.Sql;
+using Sitecore.Diagnostics.PerformanceCounters;
+using System.Reflection;
+
+namespace Sitecore.Support.Data.SqlServer
+{
+  public class SqlServerClientDataStore : Sitecore.Data.SqlServer.SqlServerClientDataStore
+  {
+    public SqlServerClientDataStore(SqlDataApi api, string objectLifetime) : base(api, objectLifetime)
+    {
+    }
+
+    public SqlServerClientDataStore(string connectionString, string objectLifetime) : base(connectionString, objectLifetime)
+    {
+    }
+
+    protected override void TouchActiveData()
+    {
+      var _cacheFieldInfo = typeof(SqlServerClientDataStore).BaseType.BaseType.GetField("_cache", BindingFlags.Instance | BindingFlags.NonPublic);
+
+      if (_cacheFieldInfo != null)
+      {
+        ICache _cache = _cacheFieldInfo.GetValue(this) as ICache;
+        if (_cache == null) return;
+
+        foreach (var key in _cache.GetCacheKeys())
+        {
+          if (!string.IsNullOrEmpty(key) && _cache[key] != null)
+          {
+            this.TouchData(key);
+            DataCount.DataClientDataWrites.Increment(1L);
+          }
+        }
+      }
+    }
+  }
+}
---
+++ src/Sitecore.Support.138194.176278/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.138194.176278")]
+[assembly: AssemblyProduct("Sitecore.Support.138194.176278")]
+[assembly: ComVisible(false)]
